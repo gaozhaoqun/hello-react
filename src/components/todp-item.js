9883feee51a4d661fa23df44bb29f35f8c8ba34e
gaozhaoqun <2855521@qq.com>
2019-03-31 20:37:26 +0800

    提交todo-item组件

---
+++ src/components/todp-item.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class TodoItem extends Component {
+    constructor(props) {
+        super(props)
+        this.handleDeleteItem = this.handleDeleteItem.bind(this)
+    }
+    handleDeleteItem() {
+        const { handleDelete, i } = this.props  // 解构赋值
+            handleDelete(i)
+    }
+    render() {
+        const { content } = this.props
+        return (
+            <div 
+                onClick={this.handleDeleteItem}
+            >
+                {content}
+            </div>
+        )
+    }
+}
+
+export default TodoItem
